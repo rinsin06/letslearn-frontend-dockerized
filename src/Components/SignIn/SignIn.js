b0feb817346870cc2ae05d88e524e5dff1da5e7f
rinsin06 <129921475+rinsin06@users.noreply.github.com>
2024-04-17 01:32:52 +0530

    Update SignIn.js

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -50,8 +50,8 @@ const SignIn = () => {
             
             const token = response.data.token;
             const refreshToken = response.data.refreshToken;
-            Cookies.set('refreshToken',refreshToken,{ expires: 10, secure: false })
-            Cookies.set('token',token,{ expires: 10, secure: false })
+            Cookies.set('refreshToken',refreshToken,{ expires: 10, secure: true })
+            Cookies.set('token',token,{ expires: 10, secure: true })
             localStorage.setItem("token", response.data.token);
             localStorage.setItem("name", response.data.name);
             localStorage.setItem("email", response.data.email);
