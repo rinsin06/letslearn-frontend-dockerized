3da5560225e00c89123db101df2d55379d54507d
rinsin06 <129921475+rinsin06@users.noreply.github.com>
2024-04-19 12:19:03 +0530

    Update UserService.js

--- src/UserService.js
+++ src/UserService.js
@@ -7,11 +7,25 @@ axios.defaults.withCredentials = true;
 
 const USER_BASE_REST_API_URL = 'https://shopifytech.xyz';
 
+const axiosInstance = axios.create({
+
+});
+
+// Add a request interceptor to add the token to headers before sending the request
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token'); // assuming token is stored in localStorage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
 export const getProfile = (input) => {
     const params = {
         username: input,
       };
-    return axios.get(USER_BASE_REST_API_URL+'/user/profile',{params:params});
+    return axiosInstance.get(USER_BASE_REST_API_URL+'/user/profile',{params:params});
 }
 
 export const getWallet = (input) =>{
@@ -22,7 +36,7 @@ export const getWallet = (input) =>{
          id:id
     }
 
-    return axios.get(USER_BASE_REST_API_URL+'/user/wallet',{params:param})
+    return axiosInstance.get(USER_BASE_REST_API_URL+'/user/wallet',{params:param})
 }
 
 
@@ -32,32 +46,32 @@ export const sendOtp = (input) =>{
         username: input,
       };
 
-    return axios.get(USER_BASE_REST_API_URL+'/user/sendotp',{params:params})
+    return axiosInstance.get(USER_BASE_REST_API_URL+'/user/sendotp',{params:params})
 }
 
 
 export const getTransactions = (id)=>{
 
-return axios.get(USER_BASE_REST_API_URL+`/user/wallet/transactions/${id}`)
+return axiosInstance.get(USER_BASE_REST_API_URL+`/user/wallet/transactions/${id}`)
 }
 
 export const createOrder = (input) =>
 {
-    return axios.get(USER_BASE_REST_API_URL+`/user/payment/${input}`)
+    return axiosInstance.get(USER_BASE_REST_API_URL+`/user/payment/${input}`)
 }
 
 
 export const addWallet = (input) =>{
 
-    return axios.post(USER_BASE_REST_API_URL+'/user/wallet/add',input)
+    return axiosInstance.post(USER_BASE_REST_API_URL+'/user/wallet/add',input)
 }
 
 export const changePassword =(input) =>{
 
-    return axios.post(USER_BASE_REST_API_URL+'/user/change-password',input)
+    return axiosInstance.post(USER_BASE_REST_API_URL+'/user/change-password',input)
 }
 
 export const verifyOtp = (input) =>{
 
-    return axios.get(USER_BASE_REST_API_URL+'/user/verify-otp',{params:input})
+    return axiosInstance.get(USER_BASE_REST_API_URL+'/user/verify-otp',{params:input})
 }
